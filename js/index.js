f18d69598903d4c89b6b0b2935df53126d293f9a
mdipace <mdipace@udesa.edu.ar>
2023-05-26 15:48:53 -0300

    estructuras de los html, linkeados al css y js respectivos, header terminado, boton modo oscuro?

--- js/index.js
+++ js/index.js
@@ -1,14 +1,39 @@
-var btnoscuro   = document.querySelector("#modo-oscuro") 
-var body        = document.querySelector("body")
+// FORMULARIO
+let form = document.querySelector("#search-button"); //HTML button element
+let input = document.querySelector("#search-input"); //HTML input element
+
+// alert(input)
+// alert (form)
+
+form.addEventListener("submit", function(e) {
+    if (input.length >= 3) {
+        form.submit();
+    } else {
+        e.preventDefault();
+        alert("Debes escribir por lo menos tres caracteres");
+    }
+})
 
 
-// btnoscuro.onclick = function(e){
-//     btnoscuro.classList.btnoscuro('active');
-//     body.classList.btnoscuro('active');
-// }
-btnoscuro.addEventListener("click", function(e){
-    let checked = e.target.checked
-    document.body.classList.btnoscuro('dark')
-        
-}) 
+// BOTON MODO OSCURO MODO CLARO //
+let btnMode        = document.querySelector("#modo-oscuro");
+let body           = document.querySelector("body");
+let bodyTitulos    = document.querySelector(".titulo");
+let bodyContainer  = document.querySelector(".box-container");
+//porque mierda no me toma los otros?!?!?!? 
+// solo me toma el cosito de canciones 
 
+
+btnMode.addEventListener("click", function (e) {
+    if (btnMode.innerText == "Modo Oscuro") {
+        body.style.background = "#313131";
+        bodyTitulos.style.backgroundColor = "#fff"
+        bodyContainer.style.backgroundColor = "#fff"
+        this.innerText = "Modo Claro";
+    } else {
+        body.style.background = "#fff";
+        bodyTitulos.style.backgroundColor = "#313131"
+        bodyContainer.style.backgroundColor = "#313131"
+        this.innerText = "Modo Oscuro";
+    }
+})
